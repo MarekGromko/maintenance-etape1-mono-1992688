2595ba04002f4ba6a1ead1031738c088d5520baf
Marek Gromko <marek.gromko@hotmail.ca>
2025-08-27 19:46:49 -0400

    Create the the item service interface

---
+++ backend/src/core/IItemService.ts
@@ -0,0 +1,13 @@
+import { Result } from "../internal/Result";
+import { Item } from "../model/Item";
+// TODO: Add methods for populating the leases
+/**
+ * Interface for the item-related service
+ */
+export abstract class IItemService {
+    abstract getItem(id: string): Promise<Result<Item, ItemServiceErr>>;
+    abstract createItem(item: Item): Promise<Result<Item, ItemServiceErr>>;
+    abstract updateItem(id: string, item: Item): Promise<Result<Item, ItemServiceErr>>;
+    abstract deleteItem(id: string): Promise<Result<void, ItemServiceErr>>;
+}
+export type ItemServiceErr = 'ITEM_NOT_FOUND' | 'ITEM_ALREADY_EXISTS' | 'INVALID_ITEM_DATA' | 'INTERNAL_ERROR';
