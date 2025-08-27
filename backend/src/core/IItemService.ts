696b7effc3621541e0323360a46d810c7cc6795f
Marek Gromko <marek.gromko@hotmail.ca>
2025-08-27 19:52:49 -0400

    Fix: Signature type error

--- backend/src/core/IItemService.ts
+++ backend/src/core/IItemService.ts
@@ -5,9 +5,9 @@ import { Item } from "../model/Item";
  * Interface for the item-related service
  */
 export abstract class IItemService {
-    abstract getItem(id: string): Promise<Result<Item, ItemServiceErr>>;
+    abstract getItem(id: number): Promise<Result<Item, ItemServiceErr>>;
     abstract createItem(item: Item): Promise<Result<Item, ItemServiceErr>>;
     abstract updateItem(item: Item): Promise<Result<Item, ItemServiceErr>>;
-    abstract deleteItem(id: string): Promise<Result<void, ItemServiceErr>>;
+    abstract deleteItem(id: number): Promise<Result<void, ItemServiceErr>>;
 }
 export type ItemServiceErr = 'ITEM_NOT_FOUND' | 'ITEM_ALREADY_EXISTS' | 'INVALID_ITEM_DATA' | 'INTERNAL_ERROR';
