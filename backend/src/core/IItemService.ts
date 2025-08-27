ff891649a105c560d7ff6aa614c61ce2bddf914e
Marek Gromko <marek.gromko@hotmail.ca>
2025-08-27 19:51:46 -0400

    FIx: updateItem function signature

--- backend/src/core/IItemService.ts
+++ backend/src/core/IItemService.ts
@@ -7,7 +7,7 @@ import { Item } from "../model/Item";
 export abstract class IItemService {
     abstract getItem(id: string): Promise<Result<Item, ItemServiceErr>>;
     abstract createItem(item: Item): Promise<Result<Item, ItemServiceErr>>;
-    abstract updateItem(id: string, item: Item): Promise<Result<Item, ItemServiceErr>>;
+    abstract updateItem(item: Item): Promise<Result<Item, ItemServiceErr>>;
     abstract deleteItem(id: string): Promise<Result<void, ItemServiceErr>>;
 }
 export type ItemServiceErr = 'ITEM_NOT_FOUND' | 'ITEM_ALREADY_EXISTS' | 'INVALID_ITEM_DATA' | 'INTERNAL_ERROR';
