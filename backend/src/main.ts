24c5d00bea8ed26c252ffef367c63cfc959b5dbd
Marek Gromko <marek.gromko@hotmail.ca>
2025-08-27 20:38:43 -0400

    create the main file

---
+++ backend/src/main.ts
@@ -0,0 +1,18 @@
+import * as core from "./core/core";
+import { hostBuilder } from "./internal/Services";
+import { SQLiteDriver } from "./data/SQLiteDriver";
+import { UserRepositoryImpl } from "./repository/UserRepositoryImpl";
+import { ItemRepositoryImpl } from "./repository/ItemRepositoryImpl";
+import { UserServiceImpl } from "./service/UserServiceImpl";
+import { ItemServiceImpl } from "./service/ItemServiceImpl";
+
+const host = hostBuilder()
+    .addSingleton(core.IDatabaseDriver, SQLiteDriver)
+    .addScope(core.IItemRepository, ItemRepositoryImpl)
+    .addScope(core.IUserRepository, UserRepositoryImpl)
+    .addScope(core.IUserService, UserServiceImpl)
+    .addScope(core.IItemService, ItemServiceImpl)
+    .build();
+
+export const makeItemServices = () => host.make(core.IItemService);
+export const makeUserServices = () => host.make(core.IUserService);
